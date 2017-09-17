16f0a7946c87b2218c932e031ada6bbd521d8b88
prezes96 <gmilaszkiewicz@gmail.com>
2017-09-18 01:53:39 +0200

    Stworzenie głównego menu

--- app/src/main/java/prezes/jedzenie/MainActivity.java
+++ app/src/main/java/prezes/jedzenie/MainActivity.java
@@ -17,17 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-//first commit, test update
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void klik(View view) {
+        switch(view.getId())
+        {
+            case R.id.przyciskSniadanie:
+
+                break;
+            case R.id.przyciskObiad:
+
+                break;
+            case R.id.przyciskKolacja:
+
+                break;
+            case R.id.przyciskLista:
+
+                break;
+        }
     }
 }
